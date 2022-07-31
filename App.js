ace73bb8213325c77dd0b34c98cb2b49c0dc5a68
nicop <nicopironio@gmail.com>
2022-07-30 23:07:11 -0300

    add dropdowns

--- App.js
+++ App.js
@@ -2,28 +2,48 @@ import React, { useState } from "react";
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Button, Vibration, Text, TextInput, View } from 'react-native';
 import Slider from '@react-native-community/slider';
+import DropDownPicker from 'react-native-dropdown-picker';
 
 
 
-function event_scale(scale, duration) {
+function pulse_scale(scale, duration) {
   return Math.round(scale * duration)
+};
+
+const Dropdown = () => {
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState("Strong");
+  const [items, setItems] = useState([{label: 'Weak', value: 'Weak'},
+                                      {label: "Medium", value: "Medium"},
+                                      {label: "Strong", value: "Strong"}]);
+
+  return (
+    <DropDownPicker
+      open={open}
+      value={value}
+      items={items}
+      setOpen={setOpen}
+      setValue={setValue}
+      setItems={setItems}
+      style={{width: 60, height: 20}}
+    />
+    )
 }
 
 export default function Metronome() {
   
   const [BPM, setBPM] = useState(60)
   const [pulse_duration, setPD] = useState(50)  
-  const [strong_duration, setSD] = useState(event_scale(0.9, pulse_duration))
-  const [mid_duration, setMD] = useState(event_scale(0.7, pulse_duration))
-  const [light_duration, setLD] = useState(event_scale(0.5, pulse_duration))
+  
+  const [strong_scale, setSD] = useState(0.9)
+  const [mid_scale, setMD] = useState(0.7)
+  const [weak_scale, setLD] = useState(0.5)
 
-  let create_pulse = function(vibration_duration) {
-    let pulse = [Math.round(60*1000/BPM) - vibration_duration, vibration_duration] 
-    console.log(pulse)
-    return pulse
+  let create_pulse = function(vibration_scale) {
+    let vibration_duration = pulse_scale(vibration_scale, pulse_duration)
+    return [Math.round(60*1000/BPM) - vibration_duration, vibration_duration]
   }
   
-
   return (
     <View style={styles.container}>
       <Text> BPM </Text>
@@ -34,39 +54,46 @@ export default function Metronome() {
         maximumValue={300}
         step={1}
         onValueChange={newBPM => {
+          Vibration.cancel()
           setBPM(newBPM);
         }
       }
       style={{"width": 300, "height": 20}}
       />
+    <Text> Beat accents </Text>
+{/*    <View style={{flexDirection:'row', alignItems:'center', justifyContent:'space-between'}}>
+      <Dropdown />
+      <Dropdown />
+      <Dropdown />
+      <Dropdown />
+    </View>*/}
+    <Button
+      onPress={() => {    
+            Vibration.vibrate(create_pulse(strong_scale), true); 
+        }}
+      title={"Strong"}
+    />
 
-      <Button
-        onPress={() => {    
-              Vibration.vibrate(create_pulse(strong_duration), true); 
-          }}
-        title={"Strong"}
-      />
+    <Button
+      onPress={() => {    
+            Vibration.vibrate(create_pulse(mid_scale), true); 
+        }}
+      title={"Medium"}
+    />
 
-      <Button
-        onPress={() => {    
-              Vibration.vibrate(create_pulse(mid_duration), true); 
-          }}
-        title={"Medium"}
-      />
+    <Button
+      onPress={() => {    
+            Vibration.vibrate(create_pulse(weak_scale), true); 
+        }}
+      title={"weak"}
+    />
 
-      <Button
-        onPress={() => {    
-              Vibration.vibrate(create_pulse(light_duration), true); 
-          }}
-        title={"Light"}
-      />
-
-      <Button
-        onPress={() => {
-            Vibration.cancel();  
-          }}
-        title={"Stop"}
-      />
+    <Button
+      onPress={() => {
+          Vibration.cancel();  
+        }}
+      title={"Stop"}
+    />
       <StatusBar style="auto" />
     </View>
   );
